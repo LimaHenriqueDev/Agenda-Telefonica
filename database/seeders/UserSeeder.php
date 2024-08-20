7dc84ffcb0268c77ebce87a2f827c872c8a34340
José Oliveira <henriquelimasousadev@gmail.com>
2024-08-20 15:16:38 -0300

    Feat: Cria seeders relacionada e migrations relacionado ao contato e imagem

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Cria um usuário administrador
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // Cria alguns usuários de teste
+        User::factory()->count(10)->create();
+    }
+}
